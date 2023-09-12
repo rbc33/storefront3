59f2b3d12b9ac0c3fac2b318727045057ee20104
rbc33 <ricardobenthem@gmail.com>
2023-09-12 07:10:26 +0200

    uploading files

--- store/models.py
+++ store/models.py
@@ -22,6 +22,7 @@ class Collection(models.Model):
         ordering = ['title']
 
 
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
@@ -42,6 +43,9 @@ class Product(models.Model):
     class Meta:
         ordering = ['title']
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')       
+    image = models.ImageField(upload_to='store/images')
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
